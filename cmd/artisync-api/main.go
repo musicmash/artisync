dac97c1a5a787e7b8c34436e1823986cd7315863
Mikhail Kalinin <objque@gmail.com>
2020-10-19 16:58:33 +0300

    Add example usage of db package

--- cmd/artisync-api/main.go
+++ cmd/artisync-api/main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"bufio"
+	"context"
+	"database/sql"
+	"fmt"
 	"os"
 
+	"github.com/musicmash/artisync/internal/db"
+	"github.com/musicmash/artisync/internal/db/models"
 	"github.com/musicmash/artisync/internal/log"
 	"github.com/musicmash/artisync/internal/version"
 )
@@ -13,7 +18,35 @@ func main() {
 	log.SetWriters(log.GetConsoleWriter())
 
 	log.Debug(version.FullInfo)
-	log.Info("hello, world")
+
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"),
+	)
+	mgr, err := db.Connect(dsn)
+	if err != nil {
+		log.Error(err.Error())
+		os.Exit(2)
+	}
+
+	err = mgr.ExecTx(context.Background(), func(querier *models.Queries) error {
+		art, err := querier.CreateArtist(context.Background(), models.CreateArtistParams{
+			Name:   "rammstein",
+			Poster: sql.NullString{},
+		})
+		if err != nil {
+			return err
+		}
+
+		log.Infof("id: %d, created_at: %v", art.ID, art.CreatedAt.String())
+		return nil
+	})
+	if err != nil {
+		log.Errorf("got error after tx: %s", err.Error())
+		os.Exit(2)
+	}
+
 	reader := bufio.NewReader(os.Stdin)
 	_, _ = reader.ReadString('\n')
 }
