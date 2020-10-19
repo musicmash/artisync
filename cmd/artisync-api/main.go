9102212e66afb653c052bb81fb585a8d52f640d7
Mikhail Kalinin <objque@gmail.com>
2020-10-20 01:10:06 +0300

    Extract test pipeline into post controller of healthz package

--- cmd/artisync-api/main.go
+++ cmd/artisync-api/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"database/sql"
 	"flag"
 	"fmt"
 	"net/http"
@@ -14,7 +13,6 @@ import (
 	"github.com/musicmash/artisync/internal/api"
 	"github.com/musicmash/artisync/internal/config"
 	"github.com/musicmash/artisync/internal/db"
-	"github.com/musicmash/artisync/internal/db/models"
 	"github.com/musicmash/artisync/internal/log"
 	"github.com/musicmash/artisync/internal/version"
 )
@@ -49,40 +47,6 @@ func main() {
 	err = mgr.ApplyMigrations(conf.DB.MigrationsDir)
 	if err != nil && err != migrate.ErrNoChange {
 		exitIfError(fmt.Errorf("cant-t apply migrations: %v", err))
-	}
-
-	log.Info("creating artists and others...")
-	err = mgr.ExecTx(context.Background(), func(querier *models.Queries) error {
-		art, err := querier.CreateArtist(context.Background(), models.CreateArtistParams{
-			Name:   "rammstein",
-			Poster: sql.NullString{},
-		})
-		if err != nil {
-			return fmt.Errorf("can't create new artist: %w", err)
-		}
-
-		_, err = querier.CreateArtistAssociation(context.Background(), models.CreateArtistAssociationParams{
-			ArtistID:  art.ID,
-			StoreName: "spotify",
-			StoreID:   "059c3940-a791-422d-8330-2954918c51e6",
-		})
-		if err != nil {
-			return fmt.Errorf("can't associate artist: %w", err)
-		}
-
-		err = querier.CreateSubscription(context.Background(), models.CreateSubscriptionParams{
-			UserName:  "objque",
-			StoreName: "spotify",
-			StoreID:   "059c3940-a791-422d-8330-2954918c51e6",
-		})
-		if err != nil {
-			return fmt.Errorf("can't subscribe user: %w", err)
-		}
-
-		return nil
-	})
-	if err != nil {
-		exitIfError(fmt.Errorf("got error after tx: %s", err.Error()))
 	}
 
 	router := api.GetRouter(mgr)
