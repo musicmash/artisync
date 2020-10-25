3126548c492224ec5f587a39867627926f713da9
Mikhail Kalinin <objque@gmail.com>
2020-10-25 14:56:35 +0300

    Fix query for creating daily_task

--- internal/db/models/daily_tasks.sql.go
+++ internal/db/models/daily_tasks.sql.go
@@ -10,6 +10,7 @@ import (
 const createDailySyncTask = `-- name: CreateDailySyncTask :exec
 INSERT INTO artist_daily_sync_tasks (user_name)
 VALUES ($1)
+ON CONFLICT DO UPDATE SET updated_at=now()
 `
 
 func (q *Queries) CreateDailySyncTask(ctx context.Context, userName string) error {
