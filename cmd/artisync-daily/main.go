3f4c66bd3e414baec54b724a5c6250de6c848dc4
Mikhail Kalinin <objque@gmail.com>
2020-10-28 00:51:20 +0300

    Connect to the db

--- cmd/artisync-daily/main.go
+++ cmd/artisync-daily/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"os"
@@ -9,8 +10,10 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/golang-migrate/migrate/v4"
 	"github.com/musicmash/artisync/internal/config"
 	"github.com/musicmash/artisync/internal/cron"
+	"github.com/musicmash/artisync/internal/db"
 	"github.com/musicmash/artisync/internal/log"
 	"github.com/musicmash/artisync/internal/services/scheduletask"
 	"github.com/musicmash/artisync/internal/version"
@@ -34,6 +37,20 @@ func main() {
 	log.SetWriters(log.GetConsoleWriter())
 
 	log.Debug(version.FullInfo)
+
+	log.Info("connecting to db...")
+	mgr, err := db.Connect(conf.DB.GetConnString())
+	exitIfError(err)
+
+	log.Info("connection to the db established")
+
+	if conf.DB.AutoMigrate {
+		log.Info("applying migrations..")
+		err = mgr.ApplyMigrations(conf.DB.MigrationsDir)
+		if !errors.Is(err, migrate.ErrNoChange) {
+			exitIfError(fmt.Errorf("cant-t apply migrations: %w", err))
+		}
+	}
 
 	task := scheduletask.New()
 
