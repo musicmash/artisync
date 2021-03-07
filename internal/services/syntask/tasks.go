dc3ef89b4d36e5299f3eb7fee7f4df996fde3952
Mikhail Kalinin <objque@gmail.com>
2021-03-08 01:58:30 +0300

    Check if there is already sync task processing for user

--- internal/services/syntask/tasks.go
+++ internal/services/syntask/tasks.go
@@ -5,7 +5,6 @@ import (
 	"database/sql"
 	"encoding/json"
 	"errors"
-	"fmt"
 
 	"github.com/google/uuid"
 	"github.com/musicmash/artisync/internal/db"
@@ -44,38 +43,29 @@ func (m *Mgr) GetSyncTaskState(ctx context.Context, id uuid.UUID) (*Task, error)
 }
 
 func (m *Mgr) GetOrCreateOneTimeSyncTaskForUser(ctx context.Context, userName string, code string) (*Task, error) {
-	return m.getOrCreateSyncTaskForUser(ctx, false, userName, code)
+	const scheduledDaily = false
+
+	return m.getOrCreateSyncTaskForUser(ctx, scheduledDaily, userName, code)
 }
 
 func (m *Mgr) GetOrCreateDailySyncTaskForUser(ctx context.Context, userName string, code string) (*Task, error) {
-	return m.getOrCreateSyncTaskForUser(ctx, true, userName, code)
+	const scheduledDaily = true
+
+	return m.getOrCreateSyncTaskForUser(ctx, scheduledDaily, userName, code)
 }
 
 //nolint:lll
 func (m *Mgr) getOrCreateSyncTaskForUser(ctx context.Context, scheduleDaily bool, userName string, code string) (*Task, error) {
-	// if any sync sync task already spawned, but not finished
-	res, err := m.conn.IsAnySyncTaskProcessingForUser(ctx, userName)
-	if err == nil {
-		task := Task{ID: res.ID, State: string(res.State), Details: res.Details}
-		return &task, nil
+	task, err := m.pipeline.Run(ctx, &syntask.PipelineOpts{
+		UserName:          userName,
+		SpotifyAuthCode:   code,
+		ScheduleDailySync: scheduleDaily,
+	})
+	if err != nil {
+		// TODO (m.kalinin): check if spotify code is broken?
+		return nil, guard.NewInternalError(err)
 	}
 
-	// task not found
-	if errors.Is(err, sql.ErrNoRows) {
-		task, err := m.pipeline.Run(ctx, &syntask.PipelineOpts{
-			UserName:          userName,
-			SpotifyAuthCode:   code,
-			ScheduleDailySync: scheduleDaily,
-		})
-		if err != nil {
-			// TODO (m.kalinin): check if spotify code is broken?
-			return nil, guard.NewInternalError(err)
-		}
-
-		result := Task(*task)
-		return &result, nil
-	}
-
-	// another error here
-	return nil, guard.NewInternalError(fmt.Errorf("can't get current tasks for %v: %w", userName, err))
+	result := Task(*task)
+	return &result, nil
 }
